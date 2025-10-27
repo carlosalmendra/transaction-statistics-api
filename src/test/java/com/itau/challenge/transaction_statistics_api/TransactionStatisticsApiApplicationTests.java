8d23c02bd64f657b89f77c413529700148d5bdfb
carlosalmendra <almendracarloshenrique@gmail.com>
2025-10-27 20:58:58 -0300

    first commit

---
+++ src/test/java/com/itau/challenge/transaction_statistics_api/TransactionStatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.challenge.transaction_statistics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStatisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
