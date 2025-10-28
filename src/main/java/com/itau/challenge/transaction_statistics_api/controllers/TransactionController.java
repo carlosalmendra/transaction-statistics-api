d6f686b7aaf4a3ad002a6b2d8454315eeed6e62e
carlosalmendra <almendracarloshenrique@gmail.com>
2025-10-27 21:18:49 -0300

    feat: added TransactionController

---
+++ src/main/java/com/itau/challenge/transaction_statistics_api/controllers/TransactionController.java
@@ -0,0 +1,9 @@
+package com.itau.challenge.transaction_statistics_api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/transactions")
+public class TransactionController {
+}
