c2028b5f970797173d7289bac57f8de171ef5b34
carlosalmendra <almendracarloshenrique@gmail.com>
2025-10-27 21:42:27 -0300

    feat: add POST /transaction endpoint in TransactionController

--- src/main/java/com/itau/challenge/transaction_statistics_api/controllers/TransactionController.java
+++ src/main/java/com/itau/challenge/transaction_statistics_api/controllers/TransactionController.java
@@ -1,9 +1,25 @@
 package com.itau.challenge.transaction_statistics_api.controllers;
 
+import com.itau.challenge.transaction_statistics_api.dto.TransactionRequest;
+import com.itau.challenge.transaction_statistics_api.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/transactions")
+@RequestMapping(name = "/api")
 public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping("/transaction")
+    private ResponseEntity<Void> createTransaction(@RequestBody TransactionRequest body) {
+        transactionService.createTransaction(body);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
